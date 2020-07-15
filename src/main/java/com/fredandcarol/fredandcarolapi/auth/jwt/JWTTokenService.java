69eab5a04ac05a59797e87d6939ae63cd9cd8328
Tom <thomas.spencer@codurance.com>
2020-07-15 08:55:08 +0100

    Fail: missing some details

--- src/main/java/com/fredandcarol/fredandcarolapi/auth/jwt/JWTTokenService.java
+++ src/main/java/com/fredandcarol/fredandcarolapi/auth/jwt/JWTTokenService.java
@@ -1,13 +1,58 @@
 package com.fredandcarol.fredandcarolapi.auth.jwt;
 
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
+
+import com.google.common.collect.ImmutableMap;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Clock;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.impl.compression.GzipCompressionCodec;
+import java.util.Calendar;
+import java.util.Date;
+import org.springframework.security.core.token.Token;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JWTTokenService {
+public class JWTTokenService implements Clock {
   private static String DOT = ".";
   private static GzipCompressionCodec COMRESSION_CODEC = new GzipCompressionCodec();
 
   JWTTokenService() {
+  }
+
+  public String expiring(ImmutableMap<String, String> attributes) {
+    return newToken(attributes, 600);
+  }
+
+  private String newToken(ImmutableMap<String, String> attributes, final int expiresInSec) {
+    Date now = new Date();
+    final Claims claims = Jwts
+        .claims()
+        .setIssuer("tom")
+        .setIssuedAt(now);
+
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(now);
+    cal.add(Calendar.MINUTE, expiresInSec);
+    Date newDate = cal.getTime();
+
+    if (expiresInSec > 0) {
+      final Date expiresAt = newDate;
+      claims.setExpiration(expiresAt.toDate());
+    }
+    claims.putAll(attributes);
+
+    return Jwts
+        .builder()
+        .setClaims(claims)
+        .signWith(HS256, "secretkey")
+        .compressWith(COMRESSION_CODEC)
+        .compact();
+  }
+
+  @Override
+  public Date now() {
+    return null;
   }
 }
--- src/main/java/com/fredandcarol/fredandcarolapi/auth/jwt/TokenAuthenticationService.java
+++ src/main/java/com/fredandcarol/fredandcarolapi/auth/jwt/TokenAuthenticationService.java
@@ -5,23 +5,78 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.core.userdetails.User;
-
 
 public class TokenAuthenticationService implements UserAuthenticationService {
   private Map<String, User> users = new HashMap<>();
-  private TokenService tokenService;
+  private JWTTokenService tokenService;
 
+  private String newToken(final Map<String, String> attributes, final int expiresInSec) {
+    final DateTime now = dates.now();
+    final Claims claims = Jwts
+        .claims()
+        .setIssuer(issuer)
+        .setIssuedAt(now.toDate());
 
-  @Override
-  public Optional<User> login(String username, String password){
-    return findUserByUsername(username)
-        .filter(user -> Objects.equals(password, user.getPassword()))
-        .map(user -> tokenService.expiring(ImmutableMap.of("username", username)));
+    if (expiresInSec > 0) {
+      final DateTime expiresAt = now.plusSeconds(expiresInSec);
+      claims.setExpiration(expiresAt.toDate());
+    }
+    claims.putAll(attributes);
+
+    return Jwts
+        .builder()
+        .setClaims(claims)
+        .signWith(HS256, secretKey)
+        .compressWith(COMPRESSION_CODEC)
+        .compact();
   }
 
-  private Optional<User> findUserByUsername(String username) {
-    return Optional.ofNullable(users.get(username));
+  @Override
+  public Map<String, String> verify(final String token) {
+    final JwtParser parser = Jwts
+        .parser()
+        .requireIssuer(issuer)
+        .setClock(this)
+        .setAllowedClockSkewSeconds(clockSkewSec)
+        .setSigningKey(secretKey);
+    return parseClaims(() -> parser.parseClaimsJws(token).getBody());
+  }
+
+  @Override
+  public Map<String, String> untrusted(final String token) {
+    final JwtParser parser = Jwts
+        .parser()
+        .requireIssuer(issuer)
+        .setClock(this)
+        .setAllowedClockSkewSeconds(clockSkewSec);
+
+    // See: https://github.com/jwtk/jjwt/issues/135
+    final String withoutSignature = substringBeforeLast(token, DOT) + DOT;
+    return parseClaims(() -> parser.parseClaimsJwt(withoutSignature).getBody());
+  }
+
+  private static Map<String, String> parseClaims(final Supplier<Claims> toClaims) {
+    try {
+      final Claims claims = toClaims.get();
+      final ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+      for (final Map.Entry<String, Object> e: claims.entrySet()) {
+        builder.put(e.getKey(), String.valueOf(e.getValue()));
+      }
+      return builder.build();
+    } catch (final IllegalArgumentException | JwtException e) {
+      return ImmutableMap.of();
+    }
+  }
+
+  @Override
+  public Date now() {
+    final DateTime now = dates.now();
+    return now.toDate();
+  }
+
+  @Override
+  public Optional<User> login(String username, String password) {
+    return Optional.empty();
   }
 }
